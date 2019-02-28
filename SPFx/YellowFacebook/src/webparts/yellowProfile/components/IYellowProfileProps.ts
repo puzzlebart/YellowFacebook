b2f4f6b77261cff5d18c413c1b10ea3549472279
Stian Grepperud <stian.grepperud@puzzlepart.com>
2019-02-28 13:35:57 +0100

    YellowProfile initial commit

---
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/IYellowProfileProps.ts
@@ -0,0 +1,3 @@
+export interface IYellowProfileProps {
+  description: string;
+}
---
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IYellowProfileWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'YellowProfileWebPartStrings' {
+  const strings: IYellowProfileWebPartStrings;
+  export = strings;
+}
