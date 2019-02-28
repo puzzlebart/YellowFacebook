4cd2a9ef7b5f58d1666afade456dd501376e15ee
Håvard Berg <havard.berg@puzzlepart.com>
2019-02-28 23:57:29 +0100

    merge

--- SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
@@ -8,11 +8,7 @@ export default class Images extends React.Component<IImagesProps, {}> {
   }
 
   public render() {
-<<<<<<< HEAD
     let images = this.props.properties.images.map((item => {return <img className={styles.imagePreview} src={item}></img>}));
-=======
-    console.log(this.props.properties.images);
->>>>>>> fc208422c3384b1f78d973ede60858151089c6c6
     return (
       <div className={styles.leftComponent}>
         <h3>Images</h3>
