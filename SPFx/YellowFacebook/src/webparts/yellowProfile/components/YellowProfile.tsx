f1b4c4e02e5e668953201a570c52c8010c990a0c
Stian Grepperud <stian.grepperud@puzzlepart.com>
2019-02-28 15:47:03 +0100

    Empty fetchdata

--- SPFx/YellowFacebook/src/webparts/yellowProfile/components/YellowProfile.tsx
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/YellowProfile.tsx
@@ -3,22 +3,38 @@ import styles from './YellowProfile.module.scss';
 import { IYellowProfileProps } from './IYellowProfileProps';
 import Friends from './Friends/Friends';
 import Images from './Images/Images';
+import Presentation from './Presentation/Presentation';
 
 export default class YellowProfile extends React.Component<IYellowProfileProps, {}> {
+  constructor(props) {
+    super(props);
+  }
+
+  public async componentDidMount() {
+    await this.fetchData();
+  }
+
   public render(): React.ReactElement<IYellowProfileProps> {
     return (
       <div className={styles.yellowProfile}>
         <div className={styles.leftComponentsContainer}>
-          <div className={styles.leftComponent}>
-            <span>Bio</span>
-          </div>
+          <Presentation />
           <Images />
           <Friends />
         </div>
         <div className={styles.feed}>
-
+        {this.renderItems()}
         </div>
       </div>
     );
   }
+
+  private renderItems() {
+    return <div></div>;
+  }
+
+  private async fetchData() {
+
+  }
+
 }
