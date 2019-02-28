a18fdcd83ef5443d7abd432f062e31f8d6c2285f
Håvard Berg <havard.berg@puzzlepart.com>
2019-02-28 23:56:36 +0100

    images

--- SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
@@ -8,18 +8,11 @@ export default class Images extends React.Component<IImagesProps, {}> {
   }
 
   public render() {
-<<<<<<< HEAD
-    // let images = this.props.properties.images.split(0,9);
-    console.log(this.props.properties.images);
-=======
-    console.log(this.props.properties.images)
->>>>>>> b8616d18766240c67e224bac5d8c254f31407af3
+    let images = this.props.properties.images.map((item => {return <img className={styles.imagePreview} src={item}></img>}));
     return (
       <div className={styles.leftComponent}>
         <h3>Images</h3>
-        <img className={styles.imagePreview} src={this.props.properties.images[0]}></img>
-        <img className={styles.imagePreview} src={this.props.properties.images[1]}></img>
-        <img className={styles.imagePreview} src={this.props.properties.images[2]}></img>
+        <div>{images}</div>
       </div>
     );
   }
