789c5ffbda9b9cc6b81f064998d0b1c0ecd5a7b2
Stian Grepperud <stian.grepperud@puzzlepart.com>
2019-02-28 15:01:16 +0100

    Placeholders for profile

--- SPFx/YellowFacebook/src/webparts/yellowProfile/components/YellowProfile.tsx
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/YellowProfile.tsx
@@ -1,23 +1,24 @@
 import * as React from 'react';
 import styles from './YellowProfile.module.scss';
 import { IYellowProfileProps } from './IYellowProfileProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 
 export default class YellowProfile extends React.Component<IYellowProfileProps, {}> {
   public render(): React.ReactElement<IYellowProfileProps> {
     return (
-      <div className={ styles.yellowProfile }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
+      <div className={styles.yellowProfile}>
+        <div className={styles.leftComponentsContainer}>
+          <div className={styles.leftComponent}>
+            <span>Bio</span>
           </div>
+          <div className={styles.leftComponent}>
+            <span>Images</span>
+          </div>
+          <div className={styles.leftComponent}>
+            <span>Friends</span>
+          </div>
+        </div>
+        <div className={styles.feed}>
+
         </div>
       </div>
     );
