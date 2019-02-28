99909d43528ec0140f1020c749e33d4c120e5d5e
Håvard Berg <havard.berg@puzzlepart.com>
2019-02-28 23:32:55 +0100

    IMAGES

--- SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
@@ -8,10 +8,14 @@ export default class Images extends React.Component<IImagesProps, {}> {
   }
 
   public render() {
-    console.log(this.props.properties)
+    let images = this.props.properties.images.split(0,9);
+    console.log(this.props.properties.images)
     return (
       <div className={styles.leftComponent}>
         <h3>Images</h3>
+        <img className={styles.imagePreview} src={this.props.properties.images[0]}></img>
+        <img className={styles.imagePreview} src={this.props.properties.images[1]}></img>
+        <img className={styles.imagePreview} src={this.props.properties.images[2]}></img>
       </div>
     );
   }
