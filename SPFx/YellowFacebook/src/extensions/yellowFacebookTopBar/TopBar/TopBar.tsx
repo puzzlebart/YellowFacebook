ca5a6bfc2064f3ba71e674bed96c3889eaa13be2
Stian Grepperud <stian.grepperud@puzzlepart.com>
2019-03-01 10:16:43 +0100

    SearchBox

--- SPFx/YellowFacebook/src/extensions/yellowFacebookTopBar/TopBar/TopBar.tsx
+++ SPFx/YellowFacebook/src/extensions/yellowFacebookTopBar/TopBar/TopBar.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import styles from './TopBar.module.scss';
 import { ITopBarProps } from './ITopBarProps';
+import { SearchBox } from 'office-ui-fabric-react/lib/SearchBox';
 
 export default class TopBar extends React.Component<ITopBarProps, {}> {
   public render() {
     return (
       <div className={styles.topBar}>
+      <SearchBox className={styles.searchBox} placeholder='Search' />
       </div>
     );
   }
