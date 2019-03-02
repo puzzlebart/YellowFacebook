e8016288b0fbfce79d3bc23c6a955709b94b0a25
Stian Grepperud <stian.grepperud@puzzlepart.com>
2019-03-02 11:57:23 +0100

    ImageModal

--- SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
+++ SPFx/YellowFacebook/src/webparts/yellowProfile/components/Images/Images.tsx
@@ -1,26 +1,44 @@
 import * as React from 'react';
-import styles from '../YellowProfile.module.scss';
+import styles from './Images.module.scss';
 import IImagesProps from './IImagesProps';
 import { Icon } from 'office-ui-fabric-react/lib/Icon';
+import ImageModal from './ImageModal/ImageModal';
 
-export default class Images extends React.Component<IImagesProps, {}> {
+export default class Images extends React.Component<IImagesProps, { displayImageModal: boolean, imageToShow?: string }> {
   constructor(props) {
     super(props);
+
+    this.state = {
+      displayImageModal: false
+    };
+  }
+
+  public componentDidUpdate() {
+    console.log(this.state.displayImageModal);
   }
 
   public render() {
     return (
       <div className={styles.leftComponent}>
-        <h3><Icon iconName="Photo2" className={styles.headerIcon}/>Images</h3>
+        <h3><Icon iconName="Photo2" className={styles.headerIcon} />Images</h3>
         {this.renderImages()}
+        {(this.state.displayImageModal) && <ImageModal closeModal={this._closeModal} image={this.state.imageToShow} />}
       </div>
     );
   }
 
   private renderImages() {
-   return this.props.properties.images.slice(0,9).map(image => {
-    return <img className={styles.imagePreview} src={image}></img>;
-   });
+    return this.props.properties.images.slice(0, 9).map(image => {
+      return (
+        <div className={styles.imageContainer} onClick={() => this.setState({ displayImageModal: true, imageToShow: image })}>
+          <img className={styles.imagePreview} src={image} />
+        </div>
+      );
+    });
+  }
+
+  private _closeModal = () => {
+    this.setState({ displayImageModal: false });
   }
 
 }
